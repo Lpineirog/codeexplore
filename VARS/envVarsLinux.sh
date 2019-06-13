472a49496386dba104feadb225e129d017cdc5bc
Lorena Piñeiro Germade <lorena.pineiro@atmira.com>
2019-06-13 17:21:27 +0200

    first commit

---
+++ VARS/envVarsLinux.sh
@@ -0,0 +1,8 @@
+#### VARIABLES DE ENTORNO PARA TERRAFORM ENTORNO CODEEXPLORE ######
+export TF_VAR_user_ocid=""
+export TF_VAR_compartment_ocid=""
+export TF_VAR_compartment_name="codeexplore"
+export TF_VAR_fingerprint=""
+export TF_VAR_region="eu-frankfurt-1"
+export TF_VAR_tenancy=""
+export TF_VAR_tenancy_ocid=""
